a8bdc84d41dece589dc4fcb8a4bc2b01f4e199ab
Alexei White <alexei.white@gmail.com>
2015-07-17 19:59:08 -0700

    JSONP Wrapping is working

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# gulp-jsonp
-JSONP Wrapper for Gulp
+Gulp JSONP plugin
+===================
+Simple JSONP wrapper Gulp Plugin.
+###Installation &nbsp;  [![npm version](https://badge.fury.io/js/gulp-jsonp.svg)](http://badge.fury.io/js/gulp-jsonp)
+```sh
+npm install gulp-jsonp
+```
+###Simple Usage
+```javascript
+var gjsonp = require("gulp-jsonp");
+
+/**
+ * Build JS
+ */
+gulp.task('js', function () {
+  gulp.src(['./src/**/*.html'])
+    .pipe(gjsonp({
+        callback: "__myJSONPCallbackFunction"
+      }))
+    .pipe(gulp.dest('./dist/'));
+});
+```
+###Output
+The plugin will rename files to have a .js extension, and wrap the contents in an object like so:
+```javascript
+__myJSONPCallbackFunction({"filenane":"index.html","contents":"[BASE64 ENCODED CONTENTS]"});
+```
+
--- index.js
+++ index.js
@@ -3,8 +3,20 @@
 var through = require('through2');
 var rs = require('replacestream');
 var istextorbinary = require('istextorbinary');
+var btoa = require('btoa'),
+  Path = require('path');
+
+function parsePath(path) {
+  var extname = Path.extname(path);
+  return {
+    dirname: Path.dirname(path),
+    basename: Path.basename(path, extname),
+    extname: extname
+  };
+}
 
 module.exports = function (config) {
+  config.skipBinary = true;
   var doReplace = function (file, enc, callback) {
     if (file.isNull()) {
       return callback(null, file);
@@ -17,22 +29,29 @@ module.exports = function (config) {
       }
 
       if (file.isBuffer()) {
-        if (search instanceof RegExp) {
-          file.contents = new Buffer(String(file.contents).replace(search, replacement));
-        }
-        else {
-          var cnts = String(file.contents);
+        var parsedPath = parsePath(file.relative),
+          cnts = String(file.contents),
+          fname = file.path.substr(file.path.lastIndexOf('/') + 1);
 
+        var resobj = {
+          filename: fname,
+          contents: btoa(cnts)
+        };
 
-          file.contents = new Buffer(cnts);
-        }
+        var path = file.path.substr(0, file.path.lastIndexOf('/') + 1) + Path.join(parsedPath.dirname, parsedPath.basename + parsedPath.extname.replace(/\./gi, '___') + '.js');
+        file.path = path;
+        //console.log(file.path, path);
+        //console.log(parsedPath, path);
+
+        file.contents = new Buffer(config.callback + '(' + JSON.stringify(resobj) + ');');
+
         return callback(null, file);
       }
 
       callback(null, file);
     }
 
-    if (options && options.skipBinary) {
+    if (config && config.skipBinary) {
       istextorbinary.isText('', file.contents, function (err, result) {
         if (err) {
           return callback(err, file);
